8b08e6d1245e3b211094c478151f2281b2981031
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2021-09-02 13:42:22 +0700

    Create Header Component and put to arrange LandingPage
    also remove some file in scss

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import Header from "parts/Header"
+import { Component } from "react"
+
+export default class LandingPage extends Component {
+    render() {
+      return (
+        <>
+          <Header {...this.props} />
+        </>
+      );
+    }
+};
+
